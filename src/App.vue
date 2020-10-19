d5d5bfc0a09d02a50d6863d62defe2061998b6c7
Your Name <mmuzasantuna@gmail.com>
2020-10-19 16:51:13 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDo's</h1>
+    <form v-on:submit.prevent="saveNewTodo">   
+      <input id="new-item" type="text" v-model="newTodo.name">
+
+      <label for="high">High</label>
+      <input type="radio" name="priority" v-model="newTodo.priority" value="high">
+
+      <label for="low">Low</label>
+      <input type="radio" name="priority" v-model="newTodo.priority" value="low">
+
+      <input type="submit" value="Save ToDo">
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" v-bind:key="index" v-bind:class="todo.priority">
+        <span>{{todo.name}}</span>
+      </li>
+    </ul>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        {name:"Buy shopping", priority: "high"},
+        {name:"Clean bathroom", priority: "low"},
+        {name:"Car's MOT", priority: "high"}
+        ],
+
+      newTodo: {
+        name:"",
+        priority:""
+      }
+    }
+  },
+  methods: {
+    saveNewTodo: function() {
+      this.todos.push({
+        name: this.newTodo.name,
+        priority: this.newTodo.priority
+        });
+      this.newTodo = { 
+        name: "",
+        priority: ""
+      }
+    }
   }
 }
 </script>
 
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 60%;
+  margin: 0 auto;
+  font-family: 'Lato', sans-serif;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  margin: 20px 0 0 0;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+li span {
+  padding: 8px;
+}
+
+li button {
+  background: #f2360c;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+li.low {
+  border: 2px solid #1a681e;
+  color: #1a681e;
+}
+
+li.high {
+  border: 2px solid #f2360c;
+}
+
+input[type="text"] {
+  padding: 10px;
+  width: 50%;
+  margin:5px;
+}
+
+button, input[type="submit"]{
+  padding: 10px;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #000;
 }
 </style>
